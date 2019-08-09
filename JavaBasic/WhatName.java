2a6a9f2e9ee4b3d5439ff93d92f23bb5d086ace1
erinyoonchoi <thisiserinchoi@gmail.com>
2019-08-09 18:18:22 +0900

    190809 18:16

--- JavaBasic/WhatName.java
+++ JavaBasic/WhatName.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 class WhatName{
       public static void main(String[] args){
-        System.out.println("Hello"); 
-        System.out.println("World"); 
-    }
+
+        System.out.println("이름이 뭔가요?"); 
+        String name = "";
+        
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.next();
+        
+        System.out.println("제 이름은 "+ name +"입니다."); 
+        System.out.println("몇살 이예요?");
+        int age = 0;
+        age = scanner.nextInt();
+        System.out.println("제 나이는" +age+"입니다.");
+      }
 }
